7b1356e957385b5fafe9accbc66a3437054b103c
mariasolegallina <gallinamariasole@gmail.com>
2024-04-14 13:57:22 +0200

    add ComicController + route

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -11,7 +12,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        // $comics = Comic::all();
+        // dd($comics);
     }
 
     /**
