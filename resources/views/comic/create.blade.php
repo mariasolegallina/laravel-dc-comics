42a1ae9dbcc3dc9216d58179aacc386f2b13f7f3
mariasolegallina <gallinamariasole@gmail.com>
2024-04-15 15:56:07 +0200

    add form in comic.create view

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -2,6 +2,55 @@
 
 @section('content')
 <div class="container my-3">
-    <h1 class="fs-4">Aggiungi un fumetto</h1>
+    <h1 class="fs-4 mb-4">Aggiungi un fumetto</h1>
+
+    <form>
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" name="title" id="title" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrizione</label>
+            <textarea type="text" class="form-control" name="description" id="description" required></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="thumb" class="form-label">Immagine</label>
+            <input type="text" class="form-control" name="thumb" id="thumb" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="text" class="form-control" name="price" id="price" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="series" class="form-label">Serie</label>
+            <input type="text" class="form-control" name="series" id="series" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="sale_date" class="form-label">Data di pubblicazione</label>
+            <input type="date" class="form-control" name="sale_date" id="sale_date" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="type" class="form-label">Tipo</label>
+            <input type="text" class="form-control" name="type" id="type" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="artists" class="form-label">Artisti</label>
+            <input type="text" class="form-control" name="artists" id="artists">
+        </div>
+
+        <div class="mb-3">
+            <label for="writers" class="form-label">Scrittori</label>
+            <input type="text" class="form-control" name="writers" id="writers">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Crea</button>
+      </form>
 </div>
 @endsection
