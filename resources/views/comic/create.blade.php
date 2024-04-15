c6bd41036da4b4fb2a606455a50c6afa03f7e9b7
mariasolegallina <gallinamariasole@gmail.com>
2024-04-15 15:27:28 +0200

    style changes of comic.index+show - add create view

---
+++ resources/views/comic/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container my-3">
+    <h1 class="fs-4">Aggiungi un fumetto</h1>
+</div>
+@endsection
--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="comics">
     <div class="container my-3">
-        <div class="row row-cols-3 row-gap-3">
+        <div class="row row-cols-4 row-gap-3">
             @foreach ($comics as $comic)
             <div class="col d-flex align-items-stretch">
-                <div class="card">
-                    <img class="comic-cover" src="{{ $comic->thumb }}">
-                    <div class="card-body ">
-                        <h2 class="fs-4 fw-bold">{{ $comic->title }}</h2>
-                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Visualizza</a>
-                    </div>
+                <div class="card card-body">
+                    <h2 class="fs-5 fw-bold flex-grow-1 pb-1">{{ $comic->title }}</h2>
+                    <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Visualizza</a>
                 </div>
             </div>
             @endforeach
-
+            <div class="d-grid gap-2 d-md-block">
+                <a href="{{ route('comics.create') }}" class="btn btn-secondary">Aggiungi</a>
+                <a href="#" class="btn btn-danger">Elimina</a>
+            </div>
         </div>
     </div>
-    </div>
-</div>
 @endsection
