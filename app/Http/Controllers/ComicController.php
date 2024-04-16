6c22a98745397a282359072fc4c89dde9aa0887b
mariasolegallina <gallinamariasole@gmail.com>
2024-04-16 09:47:03 +0200

    add - ComicController update

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -73,9 +73,21 @@ class ComicController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $comic->title = $request->title;
+        $comic->description = $request->description;
+        $comic->thumb = $request->thumb;
+        $comic->price = $request->price;
+        $comic->series = $request->series;
+        $comic->sale_date = $request->sale_date;
+        $comic->type = $request->type;
+        $comic->artists = $request->artists;
+        $comic->writers = $request->writers;
+
+        $comic->save();
+
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
